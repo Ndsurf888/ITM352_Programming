2cf67daead37d66de39dec7944b385ede9a6286a
Noah Duran <noahduran@dhcp-168-105-22-125.wireless.manoa.hawaii.edu>
2019-10-08 10:32:53 -1000

    Create Ex1.js

---
+++ Lab8/Ex1.js
@@ -0,0 +1,15 @@
+age = 29;
+number = 0;
+for(var number = 27; number >= age; number--){
+    console.log(`are you ${number} years old?`);  
+    
+}  
+      console.log(`are you ${number} years old?`);  
+    
+
+    
+
+    
+    
+
+
