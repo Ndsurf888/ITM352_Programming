d88a590b3ceaad9f57854dbc164ad0ccaeea5e42
Noah Duran <noahduran@Noahs-MacBook-Pro.local>
2019-11-07 12:22:22 -1000

    .

--- Lab13/info_server.ex2.js
+++ Lab13/info_server.ex2.js
@@ -11,8 +11,28 @@ app.all('*', function(request, response, next) {
 app.use(myParser.urlencoded({ extended: true }));
 app.post("/process_form", function(request, response) {
     let POST = request.body;
-    response.send(POST);
+    if (typeof POST['quantity_textbox'] != 'undefined') {
+        displayPurchase(POST, response);
+        window.stop();
+    };
 });
 
 app.use(express.static('./public')); //sets up public server
 app.listen(8080, () => console.log(`listening on port 8080`));
+
+function isNonNegInt(q, returnErrors = false) {
+    errors = []; // assume no errors at first
+    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    return returnErrors ? errors : (errors.length == 0);
+}
+
+function displayPurchase(POST, response) {
+    q = POST['quantity_textbox'];
+    if (isNonNegInt(post['quantity_textbox'])) {
+        response.send(`Thank you for purchasing ${q} things!`);
+    } else {
+        response.send(`${q} is not a quantity! Press the back button and try again.`);
+    }
+}
